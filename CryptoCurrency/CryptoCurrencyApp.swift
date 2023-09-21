b37a4837aa49db0215bc823421e1edb1c72a282f
Dzmitry Kalbasin <myattention15@gmail.com>
2023-09-21 16:53:43 +0500

    Initial Commit

---
+++ CryptoCurrency/CryptoCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyApp.swift
+//  CryptoCurrency
+//
+//  Created by ForMore on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
